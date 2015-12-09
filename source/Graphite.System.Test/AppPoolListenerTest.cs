297327762088170809897b6a1177f0e4ea14da95
Maciej Szczepański <Maciej.Szczepanski@outlook.com>
2015-12-09 23:10:37 +0100

    Added testable wrappers for perfcounters

--- source/Graphite.System.Test/AppPoolListenerTest.cs
+++ source/Graphite.System.Test/AppPoolListenerTest.cs
@@ -1,34 +1,35 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using Graphite.System.Perfcounters;
 using Xunit;
 
 namespace Graphite.System.Test
 {
     public class AppPoolListenerTest
     {
+        private TestablePerformanceCounterFactory _counterFactory;
+
         public AppPoolListenerTest()
         {
             var appPoolProcess = Process.GetProcesses().Where(p => p.ProcessName.Equals("w3wp")).FirstOrDefault();
             Assert.True(appPoolProcess != null, "A runngin IIS appool is required to run this test");
+
+            _counterFactory = new TestablePerformanceCounterFactory();
         }
 
         [Fact]
         public void ReportValue_Retrieves_a_value()
         {
             //given
-            AppPoolListener listener = new AppPoolListener("test", "Process", "Working Set", new WmiCounterNameProvider(), new PerformanceCounterFactory());
+            AppPoolListener listener = new AppPoolListener("test", "Process", "Working Set", new WmiCounterNameProvider(), _counterFactory);
 
             //when
             var value = listener.ReportValue();
 
             //then
-            Assert.True(value > 0f);
+            Assert.True(_counterFactory.CreatedCounters.All(p => p.CategoryName == "Process"));
            
         }
     }
